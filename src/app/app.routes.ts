a0dfc28b6b9848ad12aa13b592f33bc21b4c4226
Alvaro Beruvides <alvaroberuvides@gmail.com>
2025-11-02 20:26:02 -0500

    First commit.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+// Import route components here
+import { Home } from '../views/Home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Home,
+  },
+];
